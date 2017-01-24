feea2db60f8d7f7a476924c4e9cf1977ca15f3f7
siddhanthaldar <siddhanthaldar14@gmail.com>
2017-01-24 18:05:14 +0530

    Add files via upload

---
+++ guessnumber.c
@@ -0,0 +1,37 @@
+/* Guess Number from 1 to 100*/
+
+#include<stdio.h>
+
+int main()
+{
+    float first=0,middle=50,last=100;
+    char c;
+
+    while(first<=last)
+    {
+        if(first==last || (last-first)<1)
+        {
+            printf("The number is %d\n",(int)last);
+            break;
+        }
+
+        printf("Is the number less than %f?\n",middle);
+        scanf("%c",&c);
+
+
+
+        if(c=='y')
+        {
+            last=middle;
+            middle=(first+last)/2;
+        }
+        else if(c=='n')
+        {
+            first=middle;
+            middle=(first+last)/2;
+        }
+
+    }
+    return 0;
+
+}
